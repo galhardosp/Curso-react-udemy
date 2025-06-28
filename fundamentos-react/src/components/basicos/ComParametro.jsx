633f85d46d39ac5544c9d3961c3c40b96609bc63
Jefferson Wrasek Galhardo Júnior <jeffersongjunior@gmail.com>
2025-06-28 18:54:39 -0300

    revendo

---
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -0,0 +1,9 @@
+
+export default function ComParametro(props){
+    return(
+       <div>
+        <h2>{props.PrimeiroComponente}</h2>
+        <h3>{props.SegundoComponente}</h3>
+       </div>
+    )
+}
